0aad8022fa6ba17e22bdcb64e3f631ef6156990e
Konstantin Yegupov <kyegupov4@gmail.com>
2018-12-11 11:20:56 +0000

    Full implementation of encoding (no tests yet)

--- src/codec.go
+++ src/codec.go
@@ -33,7 +33,7 @@ func (self ParityEncoder) PushByte(b byte) {
 	self.Write([]byte{b})
 }
 
-func (self ParityEncoder) EncodeAnyInteger(v uint64, size uintptr) {
+func (self ParityEncoder) EncodeInteger(v uint64, size uintptr) {
 	buf := make([]byte, size)
 	switch size {
 	case 2:
@@ -46,9 +46,51 @@ func (self ParityEncoder) EncodeAnyInteger(v uint64, size uintptr) {
 	self.Write(buf)
 }
 
+// compact encoding:
+// 0b00 00 00 00 / 00 00 00 00 / 00 00 00 00 / 00 00 00 00
+//   xx xx xx 00															(0 ... 2**6 - 1)		(u8)
+//   yL yL yL 01 / yH yH yH yL												(2**6 ... 2**14 - 1)	(u8, u16)  low LH high
+//   zL zL zL 10 / zM zM zM zL / zM zM zM zM / zH zH zH zM					(2**14 ... 2**30 - 1)	(u16, u32)  low LMMH high
+//   nn nn nn 11 [ / zz zz zz zz ]{4 + n}									(2**30 ... 2**536 - 1)	(u32, u64, u128, U256, U512, U520) straight LE-encoded
+
+// Rust implementation: see impl<'a> Encode for CompactRef<'a, u64>
+
+func (self ParityEncoder) EncodeUintCompact(v uint64) {
+
+	// TODO: handle numbers wide than 64 bits (byte slices?)
+
+	if v < 1<<30 {
+		if v < 1<<6 {
+			self.PushByte(byte(v) << 2)
+		} else if v < 1<<14 {
+			binary.Write(*self.writer, binary.LittleEndian, uint16(v<<2)+1)
+		} else {
+			binary.Write(*self.writer, binary.LittleEndian, uint32(v<<2)+2)
+		}
+	}
+
+	n := byte(0)
+	limit := uint64(1 << 38)
+	for v > limit && limit > 256 { // when overflows, limit will be < 256
+		n++
+		limit <<= 8
+	}
+	if n > 4 {
+		panic("Assertion error: n>4 needed to compact-encode uint64")
+	}
+	self.PushByte((n << 2) + 3)
+	buf := make([]byte, 8)
+	binary.LittleEndian.PutUint64(buf, v)
+	self.Write(buf[:4+n])
+}
+
 func (self ParityEncoder) Encode(value interface{}) {
 	t := reflect.TypeOf(value)
 	switch t.Kind() {
+
+	// Boolean and numbers are trivially encoded via binary.Write
+	// It will use reflection again and take a performance hit
+	// TODO: consider handling every case directly
 	case reflect.Bool:
 		fallthrough
 	case reflect.Int8:
@@ -77,23 +119,54 @@ func (self ParityEncoder) Encode(value interface{}) {
 		fallthrough
 	case reflect.Float64:
 		binary.Write(*self.writer, binary.LittleEndian, value)
+
+	// Pointer in Go is nullable, so we are applying the same rules as there are for
+	// Option<T> in Rust implementation
 	case reflect.Ptr:
-		panic("TODO!")
+		t2 := t.Elem()
+		rv := reflect.ValueOf(value)
+		if rv.IsNil() {
+			self.PushByte(0)
+		} else {
+			dereferenced := reflect.Indirect(rv)
+			if t2.Kind() == reflect.Bool {
+				// See OptionBool in Rust implementation
+				if dereferenced.Bool() {
+					self.PushByte(2)
+				} else {
+					self.PushByte(1)
+				}
+			} else {
+				self.Encode(dereferenced.Interface())
+			}
+		}
+
+	// Arrays and slices: first compact-encode length, then each item individually
 	case reflect.Array:
 		fallthrough
 	case reflect.Slice:
 		rv := reflect.ValueOf(value)
-		len := int64(rv.Len())
-		if len > math.MaxUint32 {
+		len := rv.Len()
+		len64 := uint64(len)
+		if len64 > math.MaxUint32 {
 			panic("Attempted to serialize a collection with too many elements.")
 		}
-		self.EncodeCompact(uint32(len))
-		panic("TODO!")
-	case reflect.String:
-		panic("TODO!")
-	case reflect.Struct:
-		panic("TODO!")
+		self.EncodeUintCompact(len64)
+		for i := 0; i < len; i++ {
+			self.Encode(rv.Index(i))
+		}
 
+	// Strings are encoded as UTF-8 byte slices, just as in Rust
+	case reflect.String:
+		self.Encode([]byte(value.(string)))
+
+	case reflect.Struct:
+		encodeable := reflect.TypeOf((*ParityEncodeable)(nil)).Elem()
+		if t.Implements(encodeable) {
+			value.(ParityEncodeable).ParityEncode(self)
+		}
+
+	// Currently unsupported types
 	case reflect.Complex64:
 		fallthrough
 	case reflect.Complex128:
@@ -111,19 +184,6 @@ func (self ParityEncoder) Encode(value interface{}) {
 	case reflect.Invalid:
 		panic(fmt.Sprintf("Type %s cannot be encoded", t.Name()))
 	}
-
-	// For primitives and arrays/slices, do standard encoding
-	// For pointers, deref the pointer
-	// For structs, use ParityEncodeable
-	// Otherwise panic
-}
-
-func (self ParityEncoder) EncodeCompact(value interface{}) {
-	panic("TODO!")
-	// Use Reflection
-	// For primitives and arrays, do compact encoding
-	// For pointers, deref the pointer
-	// Otherwise panic
 }
 
 // ParityDecoder - a wraper around a Reader that allows decoding data items from a stream
@@ -156,9 +216,8 @@ func (self ParityEncoder) DecodeCompact(value interface{}) {
 }
 
 type ParityEncodeable interface {
+	// ParityEncode - encode and write this structure into a stream
 	ParityEncode(encoder ParityEncoder)
-}
-
-type ParityDecodeable interface {
-	ParityDecode(decoder ParityDecoder)
+	// ParityDecode - populate this structure from a stream (overwriting the current contents), return false on failure
+	ParityDecode(decoder ParityDecoder) bool
 }
