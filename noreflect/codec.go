45dc1f8e97fd6d38f119bca1a14d05b0105ceebb
Konstantin Yegupov <kyegupov4@gmail.com>
2018-12-29 04:38:51 +0000

    Go parity codec, version without reflection

---
+++ noreflect/codec.go
@@ -0,0 +1,339 @@
+// Copyright 2018 Jsgenesis
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package noreflect
+
+// Adapted from https://github.com/Joystream/parity-codec-go
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+)
+
+// Implementation for Parity codec in Tinygo.
+// Derived from https://github.com/paritytech/parity-codec/
+
+const maxUint = ^uint(0)
+const maxInt = int(maxUint >> 1)
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Encoder is a wrapper around a Writer that allows encoding data items to a stream.
+type Encoder struct {
+	writer io.Writer
+}
+
+// Write several bytes to the encoder.
+func (pe Encoder) Write(bytes []byte) {
+	c, err := pe.writer.Write(bytes)
+	check(err)
+	if c < len(bytes) {
+		panic(fmt.Sprintf("Could not write %d bytes to writer", len(bytes)))
+	}
+}
+
+// PushByte writes a single byte to an encoder.
+func (pe Encoder) PushByte(b byte) {
+	pe.Write([]byte{b})
+}
+
+// EncodeUintCompact writes an unsigned integer to the stream using the compact encoding.
+// A typical usage is storing the length of a collection.
+// Definition of compact encoding:
+// 0b00 00 00 00 / 00 00 00 00 / 00 00 00 00 / 00 00 00 00
+//   xx xx xx 00															(0 ... 2**6 - 1)		(u8)
+//   yL yL yL 01 / yH yH yH yL												(2**6 ... 2**14 - 1)	(u8, u16)  low LH high
+//   zL zL zL 10 / zM zM zM zL / zM zM zM zM / zH zH zH zM					(2**14 ... 2**30 - 1)	(u16, u32)  low LMMH high
+//   nn nn nn 11 [ / zz zz zz zz ]{4 + n}									(2**30 ... 2**536 - 1)	(u32, u64, u128, U256, U512, U520) straight LE-encoded
+// Rust implementation: see impl<'a> Encode for CompactRef<'a, u64>
+func (pe Encoder) EncodeUintCompact(v uint64) {
+
+	// TODO: handle numbers wide than 64 bits (byte slices?)
+	// Currently, Rust implementation only seems to support u128
+
+	if v < 1<<30 {
+		if v < 1<<6 {
+			pe.PushByte(byte(v) << 2)
+		} else if v < 1<<14 {
+			binary.Write(pe.writer, binary.LittleEndian, uint16(v<<2)+1)
+		} else {
+			binary.Write(pe.writer, binary.LittleEndian, uint32(v<<2)+2)
+		}
+		return
+	}
+
+	n := byte(0)
+	limit := uint64(1 << 32)
+	for v >= limit && limit > 256 { // when overflows, limit will be < 256
+		n++
+		limit <<= 8
+	}
+	if n > 4 {
+		panic("Assertion error: n>4 needed to compact-encode uint64")
+	}
+	pe.PushByte((n << 2) + 3)
+	buf := make([]byte, 8)
+	binary.LittleEndian.PutUint64(buf, v)
+	pe.Write(buf[:4+n])
+}
+
+// TODO: codegeneration
+
+func (pe Encoder) EncodeUint(value uint64, bytes int) {
+	mask := uint64(255)
+	for i := 0; i < bytes; i++ {
+		pe.PushByte(byte(value & mask))
+		value = value >> 8
+	}
+}
+
+func (pe Encoder) EncodeInt(value int64, bytes int) {
+	mask := int64(255)
+	for i := 0; i < bytes; i++ {
+		pe.PushByte(byte(value & mask))
+		value = value >> 8
+	}
+}
+
+func (pe Encoder) EncodeBool(b bool) {
+	if b {
+		pe.PushByte(1)
+	} else {
+		pe.PushByte(0)
+	}
+}
+
+func (pe Encoder) EncodeByteSlice(value []byte) {
+	pe.EncodeUintCompact(uint64(len(value)))
+	pe.Write(value)
+}
+
+func (pe Encoder) EncodeString(value string) {
+	pe.EncodeByteSlice([]byte(value))
+}
+
+// EncodeOption stores optionally present value to the stream.
+func (pe Encoder) EncodeOption(hasValue bool, value Encodeable) {
+	if !hasValue {
+		pe.PushByte(0)
+	} else {
+		pe.PushByte(1)
+		value.ParityEncode(pe)
+	}
+}
+
+// Decoder - a wraper around a Reader that allows decoding data items from a stream.
+// Unlike Rust implementations, decoder methods do not return success state, but just
+// panic on error. Since decoding failue is an "unexpected" error, this approach should
+// be justified.
+type Decoder struct {
+	reader io.Reader
+}
+
+// Read reads bytes from a stream into a buffer and panics if cannot read the required
+// number of bytes.
+func (pd Decoder) Read(bytes []byte) {
+	c, err := pd.reader.Read(bytes)
+	check(err)
+	if c < len(bytes) {
+		panic(fmt.Sprintf("Cannot read the required number of bytes %d, only %d available", len(bytes), c))
+	}
+}
+
+// ReadOneByte reads a next byte from the stream.
+// Named so to avoid a linter warning about a clash with io.ByteReader.ReadByte
+func (pd Decoder) ReadOneByte() byte {
+	buf := []byte{0}
+	pd.Read(buf)
+	return buf[0]
+}
+
+func (pd Decoder) DecodeUint(bytes uint) uint64 {
+	var value uint64
+	for i := uint(0); i < bytes; i++ {
+		b := uint64(pd.ReadOneByte())
+		value |= b << (i * 8)
+	}
+	return value
+}
+
+func (pd Decoder) DecodeInt(bytes uint) int64 {
+	var value uint64
+	var b uint64
+	for i := uint(0); i < bytes; i++ {
+		b = uint64(pd.ReadOneByte())
+		value |= b << (i * 8)
+	}
+	if b >= 128 {
+		for i := bytes; i < 8; i++ {
+			value |= b << 255
+		}
+	}
+	return int64(value)
+}
+
+func (pd Decoder) DecodeBool() bool {
+	return pd.ReadOneByte() > 0
+}
+
+// DecodeUintCompact decodes a compact-encoded integer. See EncodeUintCompact method.
+func (pd Decoder) DecodeUintCompact() uint64 {
+	b := pd.ReadOneByte()
+	mode := b & 3
+	switch mode {
+	case 0:
+		return uint64(b >> 2)
+	case 1:
+		r := uint64(pd.ReadOneByte())
+		r <<= 6
+		r += uint64(b >> 2)
+		return r
+	case 2:
+		buf := make([]byte, 4)
+		buf[0] = b
+		pd.Read(buf[1:4])
+		r := binary.LittleEndian.Uint32(buf)
+		r >>= 2
+		return uint64(r)
+	case 3:
+		n := b >> 2
+		if n > 4 {
+			panic("Not supported: n>4 encountered when decoding a compact-encoded uint")
+		}
+		buf := make([]byte, 8)
+		pd.Read(buf[:n+4])
+		return binary.LittleEndian.Uint64(buf)
+	default:
+		panic("Code should be unreachable")
+	}
+}
+
+func (pd Decoder) DecodeByteSlice() []byte {
+	value := make([]byte, pd.DecodeUintCompact())
+	pd.Read(value)
+	return value
+}
+
+func (pd Decoder) DecodeString() string {
+	return string(pd.DecodeByteSlice())
+}
+
+// DecodeOption decodes a optionally available value into a boolean presence field and a value.
+func (pd Decoder) DecodeOption(hasValue *bool, valuePointer Decodeable) {
+	b := pd.ReadOneByte()
+	switch b {
+	case 0:
+		*hasValue = false
+	case 1:
+		*hasValue = true
+		valuePointer.ParityDecode(pd)
+	default:
+		panic(fmt.Sprintf("Unknown byte prefix for encoded Option: %d", b))
+	}
+}
+
+// Encodeable is an interface that defines a custom encoding rules for a data type.
+// Should be defined for structs (not pointers to them).
+// See OptionBool for an example implementation.
+type Encodeable interface {
+	// ParityEncode encodes and write this structure into a stream
+	ParityEncode(encoder Encoder)
+}
+
+// See Int16Slice in tests as an example
+type EncodeableSlice interface {
+	Len() int
+	ParityEncodeElement(index int, encoder Encoder)
+}
+
+func (pe Encoder) EncodeSlice(s EncodeableSlice) {
+	len := s.Len()
+	pe.EncodeUintCompact(uint64(len))
+	for i := 0; i < len; i++ {
+		s.ParityEncodeElement(i, pe)
+	}
+}
+
+// Decodeable is an interface that defines a custom encoding rules for a data type.
+// Should be defined for pointers to structs.
+// See OptionBool for an example implementation.
+type Decodeable interface {
+	// ParityDecode populates this structure from a stream (overwriting the current contents), return false on failure
+	ParityDecode(decoder Decoder)
+}
+
+type DecodeableSlice interface {
+	Make(size int)
+	ParityDecodeElement(index int, decoder Decoder)
+}
+
+func (pd Decoder) DecodeSlice(s DecodeableSlice) {
+	l := int(pd.DecodeUintCompact())
+	s.Make(l)
+	for i := 0; i < l; i++ {
+		s.ParityDecodeElement(i, pd)
+	}
+}
+
+// OptionBool is a structure that can store a boolean or a missing value.
+// Note that encoding rules are slightly different from other "Option" fields.
+type OptionBool struct {
+	hasValue bool
+	value    bool
+}
+
+// NewOptionBoolEmpty creates an OptionBool without a value.
+func NewOptionBoolEmpty() OptionBool {
+	return OptionBool{false, false}
+}
+
+// NewOptionBool creates an OptionBool with a value.
+func NewOptionBool(value bool) OptionBool {
+	return OptionBool{true, value}
+}
+
+// ParityEncode implements encoding for OptionBool as per Rust implementation.
+func (o OptionBool) ParityEncode(encoder Encoder) {
+	if !o.hasValue {
+		encoder.PushByte(0)
+	} else {
+		if o.value {
+			encoder.PushByte(1)
+		} else {
+			encoder.PushByte(2)
+		}
+	}
+}
+
+// ParityDecode implements decoding for OptionBool as per Rust implementation.
+func (o *OptionBool) ParityDecode(decoder Decoder) {
+	b := decoder.ReadOneByte()
+	switch b {
+	case 0:
+		o.hasValue = false
+		o.value = false
+	case 1:
+		o.hasValue = true
+		o.value = true
+	case 2:
+		o.hasValue = true
+		o.value = false
+	default:
+		panic(fmt.Sprintf("Unknown byte prefix for encoded OptionBool: %d", b))
+	}
+}
---
+++ noreflect/codec_test.go
@@ -0,0 +1,209 @@
+// Copyright 2018 Jsgenesis
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package noreflect
+
+import (
+	"bytes"
+	"fmt"
+	"math"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func assertEqual(t *testing.T, a interface{}, b interface{}) {
+	if reflect.DeepEqual(a, b) {
+		return
+	}
+	t.Errorf("Received %v (type %v), expected %v (type %v)", a, reflect.TypeOf(a), b, reflect.TypeOf(b))
+}
+
+func assertPanics(code func()) {
+	panicked := false
+	assertPanicsInner(code, &panicked)
+	if !panicked {
+		panic("Panic was expected, but code executed successfully")
+	}
+}
+
+func assertPanicsInner(code func(), panicked *bool) {
+	defer func() {
+		if r := recover(); r != nil {
+			*panicked = true
+		}
+	}()
+	code()
+}
+
+func hexify(bytes []byte) string {
+	res := make([]string, len(bytes))
+	for i, b := range bytes {
+		res[i] = fmt.Sprintf("%02x", b)
+	}
+	return strings.Join(res, " ")
+}
+
+func encodeToBytes(value Encodeable) []byte {
+	var buffer = bytes.Buffer{}
+	value.ParityEncode(Encoder{&buffer})
+	return buffer.Bytes()
+}
+
+func verifyEncodingReturnDecoder(t *testing.T, encode func(pe Encoder), expectedHex string) Decoder {
+	var buffer = bytes.Buffer{}
+	encode(Encoder{&buffer})
+	assertEqual(t, hexify(buffer.Bytes()), expectedHex)
+	return Decoder{&buffer}
+}
+
+func TestSliceOfBytesEncodedAsExpected(t *testing.T) {
+	value := []byte{0, 1, 1, 2, 3, 5, 8, 13, 21, 34}
+	pd := verifyEncodingReturnDecoder(t,
+		func(pe Encoder) { pe.EncodeByteSlice(value) },
+		"28 00 01 01 02 03 05 08 0d 15 22",
+	)
+	assertEqual(t, pd.DecodeByteSlice(), value)
+}
+
+// You need to define types for your slices like this
+type Int16Slice struct{ v []int16 }
+
+func (s Int16Slice) Len() int                               { return len(s.v) }
+func (s Int16Slice) ParityEncodeElement(i int, pe Encoder)  { pe.EncodeInt(int64(s.v[i]), 2) }
+func (s *Int16Slice) Make(l int)                            { s.v = make([]int16, l) }
+func (s *Int16Slice) ParityDecodeElement(i int, pd Decoder) { s.v[i] = int16(pd.DecodeInt(2)) }
+
+func TestSliceOfInt16EncodedAsExpected(t *testing.T) {
+	value := Int16Slice{[]int16{0, 1, -1, 2, -2, 3, -3}}
+
+	pd := verifyEncodingReturnDecoder(t,
+		func(pe Encoder) { pe.EncodeSlice(value) },
+		"1c 00 00 01 00 ff ff 02 00 fe ff 03 00 fd ff",
+	)
+	var v2 Int16Slice
+	pd.DecodeSlice(&v2)
+	assertEqual(t, v2, value)
+}
+
+// OptionInt8 is an example implementation of an "Option" type, mirroring Option<u8> in Rust version.
+// Since Go does not support generics, one has to define such types manually.
+// See below for ParityEncode / ParityDecode implementations.
+type OptionInt8 struct {
+	hasValue bool
+	value    int8
+}
+
+func (o OptionInt8) ParityEncode(pe Encoder) {
+	pe.EncodeBool(o.hasValue)
+	if o.hasValue {
+		pe.EncodeInt(int64(o.value), 1)
+	}
+}
+
+func (o *OptionInt8) ParityDecode(pd Decoder) {
+	o.hasValue = pd.DecodeBool()
+	if o.hasValue {
+		o.value = int8(pd.DecodeInt(1))
+	}
+}
+
+func TestOptionInt8EncodedAsExpected(t *testing.T) {
+	tests := map[OptionInt8]string{
+		OptionInt8{true, 1}:  "01 01",
+		OptionInt8{true, -1}: "01 ff",
+		OptionInt8{false, 0}: "00",
+	}
+
+	for value, hex := range tests {
+		pd := verifyEncodingReturnDecoder(t,
+			func(pe Encoder) { value.ParityEncode(pe) },
+			hex,
+		)
+		v2 := OptionInt8{}
+		v2.ParityDecode(pd)
+		assertEqual(t, v2, value)
+	}
+}
+
+func TestOptionBoolEncodedAsExpected(t *testing.T) {
+	tests := map[OptionBool]string{
+		NewOptionBool(true):  "01",
+		NewOptionBool(false): "02",
+		NewOptionBoolEmpty(): "00",
+	}
+
+	for value, hex := range tests {
+		pd := verifyEncodingReturnDecoder(t,
+			func(pe Encoder) { value.ParityEncode(pe) },
+			hex,
+		)
+		v2 := OptionBool{}
+		v2.ParityDecode(pd)
+		assertEqual(t, v2, value)
+	}
+}
+
+type StringSlice struct{ v []string }
+
+func (s StringSlice) Len() int                               { return len(s.v) }
+func (s StringSlice) ParityEncodeElement(i int, pe Encoder)  { pe.EncodeString(s.v[i]) }
+func (s *StringSlice) Make(l int)                            { s.v = make([]string, l) }
+func (s *StringSlice) ParityDecodeElement(i int, pd Decoder) { s.v[i] = pd.DecodeString() }
+
+func TestSliceOfStringEncodedAsExpected(t *testing.T) {
+	value := StringSlice{[]string{
+		"Hamlet",
+		"Война и мир",
+		"三国演义",
+		"أَلْف لَيْلَة وَلَيْلَة‎"}}
+
+	pd := verifyEncodingReturnDecoder(t,
+		func(pe Encoder) { pe.EncodeSlice(value) },
+		"10 18 48 61 6d 6c 65 74 50 d0 92 d0 be d0 b9 d0 bd d0 b0 20 d0 "+
+			"b8 20 d0 bc d0 b8 d1 80 30 e4 b8 89 e5 9b bd e6 bc 94 e4 b9 89 bc d8 a3 d9 8e d9 84 d9 92 "+
+			"d9 81 20 d9 84 d9 8e d9 8a d9 92 d9 84 d9 8e d8 a9 20 d9 88 d9 8e d9 84 d9 8e d9 8a d9 92 "+
+			"d9 84 d9 8e d8 a9 e2 80 8e",
+	)
+	var v2 StringSlice
+	pd.DecodeSlice(&v2)
+	assertEqual(t, v2, value)
+}
+
+func TestCompactIntegersEncodedAsExpected(t *testing.T) {
+	tests := map[uint64]string{
+		0:              "00",
+		63:             "fc",
+		64:             "01 01",
+		16383:          "fd ff",
+		16384:          "02 00 01 00",
+		1073741823:     "fe ff ff ff",
+		1073741824:     "03 00 00 00 40",
+		1<<32 - 1:      "03 ff ff ff ff",
+		1 << 32:        "07 00 00 00 00 01",
+		1 << 40:        "0b 00 00 00 00 00 01",
+		1 << 48:        "0f 00 00 00 00 00 00 01",
+		1<<56 - 1:      "0f ff ff ff ff ff ff ff",
+		1 << 56:        "13 00 00 00 00 00 00 00 01",
+		math.MaxUint64: "13 ff ff ff ff ff ff ff ff"}
+
+	for value, hex := range tests {
+		pd := verifyEncodingReturnDecoder(t,
+			func(pe Encoder) { pe.EncodeUintCompact(value) },
+			hex,
+		)
+		v2 := pd.DecodeUintCompact()
+		assertEqual(t, v2, value)
+	}
+}
